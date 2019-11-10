c5ba96bd6d614113e338c6f01b3a82a83ebd654c
Jacob Kocina <Jkocina.jr@gmail.com>
2019-11-10 17:57:22 -0600

    MongoDb connection split up for reuse in routes

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,43 @@
 const express = require('express');
 const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017/todoapp';
+const url = 'mongodb://localhost:27017';
+var Todos;
 
-// Connect to MongoDB
-MongoClient.connect(url, (err,db) => {
-
-  console.log('MongoDB connected...');
-
-  if (err) throw err;
-
-  Todos = db.collection('todos');
-
-});
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-  Todos.find({})
+
+  Todos = connectMongo(url, 'todos');
+
+  Todos.find().toArray((err, todos) => {
+    if(err) {
+      return console.log(err);
+    }
+    console.log(todos);
+    res.render('index', {
+      title: "What to do",
+      todos: todos
+    });
+  })
+
 });
 
+function connectMongo(server, database) {
+  // Connect to MongoDB
+  MongoClient.connect(server, (err,client) => {
+
+    console.log('MongoDB connected...');
+
+    if (err) throw err;
+
+    var db = client.db('todoapp');
+
+    collection = db.collection(database);
+    console.log("THIS IS TODOS IN THE MONGO CONNECTION " + Todos.find());
+    return collection;
+
+  });
+}
 module.exports = router;
