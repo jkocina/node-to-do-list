7209868fab7545adb5a3408af620f6b146dd0cee
Jacob Kocina <Jkocina.jr@gmail.com>
2019-11-14 20:56:45 -0600

    mongo db is connected

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
-const url = 'mongodb://localhost:27017';
+const url = 'mongodb://localhost:27017/todoapp';
 var Todos;
 
 
@@ -9,34 +9,46 @@ var Todos;
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  Todos = connectMongo(url, 'todos');
+  Object Todos = connectMongo(url, "todoapp", "todoapp");
+
+  console.log("This is the type of the todos: " + typeof(Todos));
 
   Todos.find().toArray((err, todos) => {
+
     if(err) {
       return console.log(err);
     }
+
     console.log(todos);
+
     res.render('index', {
       title: "What to do",
       todos: todos
     });
   })
-
 });
 
-function connectMongo(server, database) {
+function connectMongo(url, database, collection) {
   // Connect to MongoDB
-  MongoClient.connect(server, (err,client) => {
+  MongoClient.connect('mongodb://localhost:27017', (err,client) => {
 
     console.log('MongoDB connected...');
 
-    if (err) throw err;
+    //Getting the db
+    let db = client.db(database);
 
-    var db = client.db('todoapp');
+    if (err) return console.log(err);
 
-    collection = db.collection(database);
-    console.log("THIS IS TODOS IN THE MONGO CONNECTION " + Todos.find());
-    return collection;
+    console.log("The type of the collection is: " + typeof(db.collection(collection)));
+
+    var coll = db.collection(collection);
+
+    coll.find().toArray((err,items) => {
+      if(err) throw err;
+      console.log("THIS IS TODOS IN THE MONGO CONNECTION " +  JSON.stringify(items));
+    });
+
+    return coll;
 
   });
 }
