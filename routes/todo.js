bb52a6ff9e207b350fc8b465a8c3e4652fb5a7c8
Jacob Kocina <Jkocina.jr@gmail.com>
2019-11-21 22:29:18 -0600

    Add delete and edit working

--- routes/todo.js
+++ routes/todo.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const MongoUtil = require('../util/mongoUtil');
 const url = 'mongodb://localhost:27017';
 
-/* GET users listing. */
+/* adds a task to the DB */
 router.post('/add', function(req, res, next) {
   console.log("Form submitted");
 
@@ -44,6 +44,7 @@ router.post('/add', function(req, res, next) {
 //Deletes a task from the database
 router.delete('/delete/:id', (req, res, next) => {
 
+  //Creating the delete query
   const query = {"_id" : MongoUtil.getObjectId(req.params.id)};
 
   //connect to the DB
@@ -70,6 +71,75 @@ router.delete('/delete/:id', (req, res, next) => {
   });
   //closing the db connection
   db.close;
+});
+
+/* redirects to the task editing screen */
+router.get('/edit/:id', function(req, res, next) {
+
+  console.log("Update form submitted");
+
+  //connecting to the mongo server
+  MongoUtil.connectToServer(url, "todoapp", (err) => {
+    if (err) console.log("There was an error connecting to the database: " + err);
+  });
+
+  //Getting the db instance
+  db = MongoUtil.getDb();
+
+  let updateQuery = {_id: MongoUtil.getObjectId(req.params.id)};
+
+  //Getting the todos collection
+  Todos = db.collection('todoapp');
+  Todos.find(updateQuery).next((err, todo) => {
+    if (err) {
+      return console.log(err);
+    }
+    res.render('edit', {
+      title: "Todo Edit",
+      todo: todo
+    });
+  });
+
+  //closing the db connection
+  db.close;
+
+});
+
+/* edits a task */
+router.post('/edit/:id', function(req, res, next) {
+
+  console.log("Update form submitted");
+
+  //connecting to the mongo server
+  MongoUtil.connectToServer(url, "todoapp", (err) => {
+    if (err) console.log("There was an error connecting to the database: " + err);
+  });
+
+  //Getting the db instance
+  db = MongoUtil.getDb();
+
+  let query = {_id: MongoUtil.getObjectId(req.params.id)};
+
+  let update = {
+    text: req.body.taskName,
+    body: req.body.taskBody
+  };
+
+  //Getting the todos collection
+  Todos = db.collection('todoapp');
+  Todos.updateOne(query, {$set:update}, (err, result) => {
+    if (err) {
+      return console.log(err);
+    }
+
+    console.log('Todo Added...');
+    res.redirect('/');
+  });
+
+
+  //closing the db connection
+  db.close;
+
 });
 
 module.exports = router;
